80b76609e723a8681719b623ba52ac559994a127
Pham Minh Tuan <theloneranger241@gmail,com>
2023-04-05 09:27:05 +0700

    migrate

---
+++ database/migrations/2023_04_03_021052_create_properties_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('total_price',18,4)->unsigned()->default();
+            $table->decimal('price_meter',18,1)->unsigned()->nullable();
+            $table->string('location')->nullable();
+            $table->string('area')->nullable();
+            $table->integer('rooms')->nullable();
+            $table->integer('bedrooms')->nullable();
+            $table->integer('bathrooms')->nullable();
+            $table->boolean('air_condition')->nullable();
+            $table->boolean('swimming_pool')->nullable();
+            $table->boolean('central_heating')->nullable();
+            $table->boolean('laundry_room')->nullable();
+            $table->boolean('gym')->nullable();
+            $table->boolean('window_covering')->nullable();
+            $table->boolean('internet')->nullable();
+            $table->boolean('is_active')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+};
