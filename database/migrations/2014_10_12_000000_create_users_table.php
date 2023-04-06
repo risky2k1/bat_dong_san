5bd8f8bdd8826722e11ea9fdd706cdc4c87521d7
Pham Minh Tuan <theloneranger241@gmail,com>
2023-04-06 11:32:39 +0700

    install subby

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,15 +19,18 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->smallInteger('role')->nullable()->default('2');
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
         DB::table('users')->insert([
-            'username' => config('auth.administrator.username'),
             'name' => 'admin',
             'email' => 'admin@gmail.com',
+            'role'=>'0',
+            'phone'=>'0329368007',
             'password' => Hash::make(config('auth.administrator.password')),
         ]);
     }
